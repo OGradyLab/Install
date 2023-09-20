8bdaa5919309b5b94d5e19a9a1da5d179b27f7d4
OGradyLab <145158368+OGradyLab@users.noreply.github.com>
2023-09-20 15:29:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,52 +1,55 @@
 #!/bin/bash
 
-# Directory to store downloaded files
-INSTALL_DIR="$HOME/OGradyLabApps"
+# Define the installation directory
+INSTALL_DIR="$HOME/PiClyde"
 
-# Create the directory
+# Create the installation directory if it doesn't exist
 mkdir -p $INSTALL_DIR
 
-# Download Python scripts
+# Download the Python scripts and icons
 wget -O $INSTALL_DIR/Ramp.py https://raw.githubusercontent.com/OGradyLab/Ramp/main/Ramp.py
 wget -O $INSTALL_DIR/Pulse.py https://raw.githubusercontent.com/OGradyLab/Pulse/main/Pulse.py
 wget -O $INSTALL_DIR/Clyde.py https://raw.githubusercontent.com/OGradyLab/Clyde/main/Clyde.py
 
-# Download icons
-wget -O $INSTALL_DIR/ramp_icon.png https://raw.githubusercontent.com/OGradyLab/Install/main/ramp_icon.png
-wget -O $INSTALL_DIR/pulse_icon.png https://raw.githubusercontent.com/OGradyLab/Install/main/pulse_icon.png
-wget -O $INSTALL_DIR/clyde_icon.png https://raw.githubusercontent.com/OGradyLab/Install/main/clyde_icon.png
+# Assuming the icons are named Ramp.png, Pulse.png, and Clyde.png and are located in the Install repository
+wget -O $INSTALL_DIR/Ramp.png https://raw.githubusercontent.com/OGradyLab/Install/main/Ramp.png
+wget -O $INSTALL_DIR/Pulse.png https://raw.githubusercontent.com/OGradyLab/Install/main/Pulse.png
+wget -O $INSTALL_DIR/Clyde.png https://raw.githubusercontent.com/OGradyLab/Install/main/Clyde.png
 
-# Make Python scripts executable
+# Make the Python scripts executable
 chmod +x $INSTALL_DIR/Ramp.py
 chmod +x $INSTALL_DIR/Pulse.py
 chmod +x $INSTALL_DIR/Clyde.py
 
-# Create .desktop files
+# Create desktop shortcuts
 echo "[Desktop Entry]
+Type=Application
 Name=Ramp
 Comment=Launch Ramp
+Icon=$INSTALL_DIR/Ramp.png
 Exec=python3 $INSTALL_DIR/Ramp.py
-Icon=$INSTALL_DIR/ramp_icon.png
 Terminal=false
-Type=Application" > $HOME/Desktop/Ramp.desktop
+Categories=Utility;" > $HOME/Desktop/Ramp.desktop
 
 echo "[Desktop Entry]
+Type=Application
 Name=Pulse
 Comment=Launch Pulse
+Icon=$INSTALL_DIR/Pulse.png
 Exec=python3 $INSTALL_DIR/Pulse.py
-Icon=$INSTALL_DIR/pulse_icon.png
 Terminal=false
-Type=Application" > $HOME/Desktop/Pulse.desktop
+Categories=Utility;" > $HOME/Desktop/Pulse.desktop
 
 echo "[Desktop Entry]
+Type=Application
 Name=Clyde
 Comment=Launch Clyde
+Icon=$INSTALL_DIR/Clyde.png
 Exec=python3 $INSTALL_DIR/Clyde.py
-Icon=$INSTALL_DIR/clyde_icon.png
 Terminal=false
-Type=Application" > $HOME/Desktop/Clyde.desktop
+Categories=Utility;" > $HOME/Desktop/Clyde.desktop
 
-# Make .desktop files executable
+# Make the desktop shortcuts executable
 chmod +x $HOME/Desktop/Ramp.desktop
 chmod +x $HOME/Desktop/Pulse.desktop
 chmod +x $HOME/Desktop/Clyde.desktop
