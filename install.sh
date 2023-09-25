3bb2c3bbb03959a4620de2e044acff6c55c05690
OGradyLab <145158368+OGradyLab@users.noreply.github.com>
2023-09-25 09:20:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,55 @@
 #!/bin/bash
 
-# Create a directory for the programs
+# Create a directory for the PiClyde programs
 mkdir -p ~/PiClyde
-
 cd ~/PiClyde
 
-# Download the programs and icons from GitHub
-wget https://raw.githubusercontent.com/OGradyLab/Install/main/Ramp.py
-wget https://raw.githubusercontent.com/OGradyLab/Install/main/Pulse.py
-wget https://raw.githubusercontent.com/OGradyLab/Install/main/Clyde.py
+# Download the Python programs
+wget https://raw.githubusercontent.com/OGradyLab/Ramp/main/Ramp.py
+wget https://raw.githubusercontent.com/OGradyLab/Pulse/main/Pulse.py
+wget https://raw.githubusercontent.com/OGradyLab/Clyde/main/Clyde.py
+
+# Make the Python programs executable
+chmod +x Ramp.py
+chmod +x Pulse.py
+chmod +x Clyde.py
+
+# Download the icons
 wget https://raw.githubusercontent.com/OGradyLab/Install/main/Ramp.png
 wget https://raw.githubusercontent.com/OGradyLab/Install/main/Pulse.png
 wget https://raw.githubusercontent.com/OGradyLab/Install/main/Clyde.png
 
-# Make the Python scripts executable
-chmod +x Ramp.py Pulse.py Clyde.py
-
-# Create desktop icons for the programs
-for program in Ramp Pulse Clyde; do
-cat > ~/Desktop/$program.desktop <<EOL
-[Desktop Entry]
+# Create desktop entries for the programs
+echo "[Desktop Entry]
 Type=Application
-Name=$program
-Exec=python3 ~/PiClyde/$program.py
-Icon=~/PiClyde/$program.png
+Name=Ramp
+Comment=Run Ramp program
+Exec=/home/pi/PiClyde/Ramp.py
+Icon=/home/pi/PiClyde/Ramp.png
 Terminal=false
-EOL
+Categories=Utility;" > ~/Desktop/Ramp.desktop
 
-# Make the .desktop files executable
-chmod +x ~/Desktop/$program.desktop
-done
+echo "[Desktop Entry]
+Type=Application
+Name=Pulse
+Comment=Run Pulse program
+Exec=/home/pi/PiClyde/Pulse.py
+Icon=/home/pi/PiClyde/Pulse.png
+Terminal=false
+Categories=Utility;" > ~/Desktop/Pulse.desktop
 
-# Notify the user
-echo "Installation complete! You should now see icons for Ramp, Pulse, and Clyde on your desktop."
+echo "[Desktop Entry]
+Type=Application
+Name=Clyde
+Comment=Run Clyde program
+Exec=/home/pi/PiClyde/Clyde.py
+Icon=/home/pi/PiClyde/Clyde.png
+Terminal=false
+Categories=Utility;" > ~/Desktop/Clyde.desktop
+
+# Make the desktop entries executable
+chmod +x ~/Desktop/Ramp.desktop
+chmod +x ~/Desktop/Pulse.desktop
+chmod +x ~/Desktop/Clyde.desktop
+
+echo "Installation completed!"
