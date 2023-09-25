976d39cb40e799151ed51cfd1c38aae41af1a58e
OGradyLab <145158368+OGradyLab@users.noreply.github.com>
2023-09-25 12:10:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,21 +16,14 @@ wget https://raw.githubusercontent.com/OGradyLab/Ramp/main/Ramp.png
 wget https://raw.githubusercontent.com/OGradyLab/Pulse/main/Pulse.png
 wget https://raw.githubusercontent.com/OGradyLab/Clyde/main/Clyde.png
 
-# Make the .py files executable
-chmod +x /home/brian/PiClyde/Ramp/Ramp.py
-chmod +x /home/brian/PiClyde/Pulse/Pulse.py
-chmod +x /home/brian/PiClyde/Clyde/Clyde.py
-
-# Create .desktop files for autostart
-mkdir -p ~/.config/autostart
-
+# Create .desktop files and place them in ~/.local/share/applications/
 echo "[Desktop Entry]
 Type=Application
 Name=Ramp
 Exec=python3 /home/brian/PiClyde/Ramp/Ramp.py
 Icon=/home/brian/PiClyde/Ramp.png
 Terminal=true
-Categories=Utility;" > ~/.config/autostart/Ramp.desktop
+Categories=Utility;" > ~/.local/share/applications/Ramp.desktop
 
 echo "[Desktop Entry]
 Type=Application
@@ -38,7 +31,7 @@ Name=Pulse
 Exec=python3 /home/brian/PiClyde/Pulse/Pulse.py
 Icon=/home/brian/PiClyde/Pulse.png
 Terminal=true
-Categories=Utility;" > ~/.config/autostart/Pulse.desktop
+Categories=Utility;" > ~/.local/share/applications/Pulse.desktop
 
 echo "[Desktop Entry]
 Type=Application
@@ -46,4 +39,18 @@ Name=Clyde
 Exec=python3 /home/brian/PiClyde/Clyde/Clyde.py
 Icon=/home/brian/PiClyde/Clyde.png
 Terminal=true
-Categories=Utility;" > ~/.config/autostart/Clyde.desktop
+Categories=Utility;" > ~/.local/share/applications/Clyde.desktop
+
+# Make the .py files executable
+chmod +x /home/brian/PiClyde/Ramp/Ramp.py
+chmod +x /home/brian/PiClyde/Pulse/Pulse.py
+chmod +x /home/brian/PiClyde/Clyde/Clyde.py
+
+# Add the applications to the LXDE panel
+echo "@lxpanel --profile LXDE-pi" >> ~/.config/lxsession/LXDE-pi/autostart
+echo "@python3 /home/brian/PiClyde/Ramp/Ramp.py" >> ~/.config/lxsession/LXDE-pi/autostart
+echo "@python3 /home/brian/PiClyde/Pulse/Pulse.py" >> ~/.config/lxsession/LXDE-pi/autostart
+echo "@python3 /home/brian/PiClyde/Clyde/Clyde.py" >> ~/.config/lxsession/LXDE-pi/autostart
+
+# Refresh the panel
+lxpanelctl restart
